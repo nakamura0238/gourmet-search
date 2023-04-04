79f4a36d4e429832eb6c758bcd1fb06634e3b5d2
中村文哉 <fumiya.nakamura0108@gmail.com>
2023-04-04 21:45:17 +0900

    ダミーデータをコメントアウト

--- src/hooks/usePresentPosition.js
+++ src/hooks/usePresentPosition.js
@@ -13,21 +13,21 @@ const usePresentPosition = () => {
         setCoords(
             {
             // 実際はこちらを使用
-            // lat: position.coords.latitude, // 緯度
-            // lng: position.coords.longitude, // 経度
+              lat: position.coords.latitude, // 緯度
+              lng: position.coords.longitude, // 経度
 
               // ダミーデータ
               // 東京都 千代田区 神田花岡町
-              lat: 35.698619, // 緯度
-              lng: 139.772908, // 経度
+              // lat: 35.698619, // 緯度
+              // lng: 139.772908, // 経度
             });
-        const address = await axios.get('/api/ReverseGeocoding');
-        // const address = await axios.get('/api/ReverseGeocoding', {
-        //   params: {
-        //     lat: position.coords.latitude,
-        //     lon: position.coords.longitude,
-        //   },
-        // });
+        // const address = await axios.get('/api/ReverseGeocoding');
+        const address = await axios.get('/api/ReverseGeocoding', {
+          params: {
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+          },
+        });
         const addressElement = address.data.Property.AddressElement;
         setPresentPosition(
             addressElement[0].Name + ' ' +
--- src/pages/api/ReverseGeocoding.js
+++ src/pages/api/ReverseGeocoding.js
@@ -9,11 +9,11 @@ import axios from 'axios';
 export default async function handler(req, res) {
   try {
     // リバースジオコーディング
-    // const query = req.query;
-    // const address = await axios.get(`https://map.yahooapis.jp/geoapi/V1/reverseGeoCoder?lat=${query.lat}&lon=${query.lon}&output=json&appid=${process.env.NEXT_PUBLIC_YAHOO_KEY}`);
+    const query = req.query;
+    const address = await axios.get(`https://map.yahooapis.jp/geoapi/V1/reverseGeoCoder?lat=${query.lat}&lon=${query.lon}&output=json&appid=${process.env.NEXT_PUBLIC_YAHOO_KEY}`);
 
     // こちらは使わない
-    const address = await axios.get(`https://map.yahooapis.jp/geoapi/V1/reverseGeoCoder?lat=35.698619&lon=139.772908&output=json&appid=${process.env.NEXT_PUBLIC_YAHOO_KEY}`);
+    // const address = await axios.get(`https://map.yahooapis.jp/geoapi/V1/reverseGeoCoder?lat=35.698619&lon=139.772908&output=json&appid=${process.env.NEXT_PUBLIC_YAHOO_KEY}`);
 
     res.status(200).json(address.data.Feature[0]);
   } catch (error) {
