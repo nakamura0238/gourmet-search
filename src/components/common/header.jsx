b0584068bd8efc75b624e6484bd1eebd8163d70a
中村文哉 <fumiya.nakamura0108@gmail.com>
2023-04-06 00:13:21 +0900

    ヘッダーデザインの変更

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from './header.module.scss';
+import Head from 'next/head';
+import styles from './Header.module.scss';
 
 /**
  * Headerコンポーネント
@@ -8,10 +9,17 @@ import styles from './header.module.scss';
  */
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <Link href='/'>
-        <h1>Gourmet Search</h1>
-      </Link>
-    </header>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet" />
+      </Head>
+      <header className={styles.header}>
+        <Link href='/'>
+          <h1>Gourmet Search</h1>
+        </Link>
+      </header>
+    </>
   );
 }
