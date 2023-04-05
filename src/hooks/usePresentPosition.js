87d6ad995bfa3aa93ee7369f1346b368b5939630
中村文哉 <fumiya.nakamura0108@gmail.com>
2023-04-05 23:05:49 +0900

    refactor:処理の抽出

--- src/hooks/usePresentPosition.js
+++ src/hooks/usePresentPosition.js
@@ -14,44 +14,49 @@ const usePresentPosition = () => {
   const [coords, setCoords] = useCoordsContext();
   const [presentPosition, setPresentPosition] = usePositionContext();
 
+  // 位置情報の取得に成功
+  const successGetPosition = async (position) => {
+    setCoords(
+        {
+          lat: position.coords.latitude, // 緯度
+          lng: position.coords.longitude, // 経度
+        });
+    const address = await axios.get('/api/ReverseGeocoding', {
+      params: {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+      },
+    });
+    const addressElement = address.data.Property.AddressElement;
+    setPresentPosition(
+        addressElement[0].Name + ' ' +
+      addressElement[1].Name + ' ' +
+      addressElement[2].Name);
+  };
+
+  // 位置情報の取得に失敗
+  const errorGetPosition =() => {
+    setPresentPosition('位置情報が取得できませんでした');
+  };
+
+  const options = {
+    timeout: 5000,
+    maximumAge: 0,
+  };
+
   const updateGeolocation = async () => {
     setCoords(undefined);
     setPresentPosition('現在地を取得中です');
     if (navigator.geolocation) { // navigatorの使用可能チェック
-      navigator.geolocation.getCurrentPosition(async (position) => {
-        setCoords(
-            {
-            // 実際はこちらを使用
-              lat: position.coords.latitude, // 緯度
-              lng: position.coords.longitude, // 経度
-
-              // ダミーデータ
-              // 東京都 千代田区 神田花岡町
-              // lat: 35.698619, // 緯度
-              // lng: 139.772908, // 経度
-            });
-        // const address = await axios.get('/api/ReverseGeocoding');
-        const address = await axios.get('/api/ReverseGeocoding', {
-          params: {
-            lat: position.coords.latitude,
-            lon: position.coords.longitude,
-          },
-        });
-        const addressElement = address.data.Property.AddressElement;
-        setPresentPosition(
-            addressElement[0].Name + ' ' +
-          addressElement[1].Name + ' ' +
-          addressElement[2].Name);
-      }, () => {
-        setPresentPosition('位置情報が取得できませんでした');
-      }, {
-        timeout: 5000,
-        maximumAge: 0,
-      });
+      navigator.geolocation.getCurrentPosition(
+          (position) => successGetPosition(position),
+          errorGetPosition,
+          options);
     } else { // navigator使用不可
       setPresentPosition('位置情報が取得できませんでした');
     }
   };
+
 
   useEffect(() => {
     let ignore = false;
